e1a8b9d8eb920249072201375857dae367d0e223
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-17 21:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const strong=document.querySelectorAll("strong")
+
 function highlight() {
-    //Write your code here
+for(let i=0;i<strong.length;i++){
+    
+    strong[i].style.color="rgb(0, 128, 0)"
 
-
-}
+}};
 
 
 function return_normal() {
-    //Write your code here
+    for(let i=0;i<strong.length;i++){
+    
+        strong[i].style.color="rgb(0, 0, 0)"
+    
+    }
 
     
-}
+};
